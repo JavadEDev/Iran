07e8ddc557bca741333865718657a07715b467db
JavadEsmati <Javad.esmatib@Gmail.com>
2026-02-01 21:23:31 +0100

    FIX toastify

--- components/ToastProvider.tsx
+++ components/ToastProvider.tsx
@@ -23,7 +23,6 @@ export function ToastProvider() {
       theme="light"
       className="toast-container"
       toastClassName="toast"
-      bodyClassName="toast-body"
       progressClassName="toast-progress"
     />
   );
